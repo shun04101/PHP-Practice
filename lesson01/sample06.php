4d5414f8b0d7f56b69fcba82e7689363db166fbc
shun04101 <nakashimashuntarou@gmail.com>
2022-04-13 14:02:25 +0900

    [Add]section1-2(~lesson14)

---
+++ lesson01/sample06.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample06</title>
+</head>
+<body>
+  <?php
+  $sam = 100+1050+200;
+  ?>
+  <p>合計金額は、<?php echo $sam; ?>円です。</p>
+  <p>税込金額は、<?php echo $sam * 1.1; ?>円です。</p>
+</body>
+</html>
