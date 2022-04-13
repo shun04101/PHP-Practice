0f7eba6f2bef22fd730967e1eb062570419a40cc
shun04101 <nakashimashuntarou@gmail.com>
2022-04-13 17:06:18 +0900

    [Add]section2 lesson15,16(while, for)

---
+++ lesson01/sample07.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample07</title>
+</head>
+<body>
+  <?php
+  $i = 1;
+
+  while ($i < 366):
+    echo $i . '日<br>';
+    $i = $i + 1;
+  endwhile;
+  ?>
+</body>
+</html>
---
+++ lesson01/sample08.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample08</title>
+</head>
+<body>
+  <?php
+  for ($i=1; $i<366; $i=$i+1):
+    echo $i . '<br>';
+  endfor;
+  ?>
+</body>
+</html>
