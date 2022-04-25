4a29ede96254a995261f51c6560fac5c467cead3
shun04101 <nakashimashuntarou@gmail.com>
2022-04-25 20:40:22 +0900

    [Add]section2 lesson24-27(ファイルの読み込み/XML/JSON/)

---
+++ lesson01_2/sample17_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>XMLファイルを読み込もう</title>
+</head>
+
+<body>
+  <?php
+  $xmlTree = simplexml_load_file('rss.xml');
+  foreach ($xmlTree->channel->item as $item) :
+  ?>
+    ・<a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a><br>
+  <?php
+  endforeach;
+  ?>
+</body>
+
+</html>
