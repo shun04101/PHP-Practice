cb58d4e54a77a75cc717e7d9328ae59da8c02a91
shun04101 <nakashimashuntarou@gmail.com>
2022-05-07 15:32:08 +0900

    [Add]section2(2nd) lesson38-41(ファイルのアップロード/独自のファンクション/外部のファイルの読み込み/MYSQL/)

---
+++ lesson01_2/sample31_2/page02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>外部ファイルを読み込む</title>
+</head>
+
+<body>
+  <?php
+  require('index.php');
+
+  echo intax(2000);
+  ?>
+</body>
+
+</html>
